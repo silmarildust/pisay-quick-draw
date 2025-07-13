6974f4ae05c18da8e33d290a679d8b311ac48410
janna vega <malin.janna.vega@gmail.com>
2025-07-13 23:00:52 +0800

    Add files via upload
    for 4 classes

---
+++ model_trainer_updated.py
@@ -0,0 +1,53 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.optimizers import Adam
+
+IMG_SIZE = 28
+BATCH_SIZE = 8
+
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=15,
+    zoom_range=0.1,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    validation_split=0.2
+)
+
+train_data = datagen.flow_from_directory(
+    'dataset',
+    target_size=(IMG_SIZE, IMG_SIZE),
+    color_mode='grayscale',
+    batch_size=BATCH_SIZE,
+    class_mode='sparse',
+    subset='training',
+    shuffle=True
+)
+
+val_data = datagen.flow_from_directory(
+    'dataset',
+    target_size=(IMG_SIZE, IMG_SIZE),
+    color_mode='grayscale',
+    batch_size=BATCH_SIZE,
+    class_mode='sparse',
+    subset='validation'
+)
+
+print(train_data.class_indices)
+
+model = Sequential([
+    Conv2D(16, (3,3), activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 1)),
+    MaxPooling2D(),
+    Conv2D(32, (3,3), activation='relu'),
+    MaxPooling2D(),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(4, activation='softmax')
+])
+
+model.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(train_data, validation_data=val_data, epochs=40)
+
+model.save("model_final.h5")
